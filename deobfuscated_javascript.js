04b5be5f78470e0d89837358c3121231a7d7e15f
Kevin Cryan <kcryan@gmail.com>
2024-05-24 18:31:43 -0400

    renamed vars/functions

--- deobfuscated_javascript.js
+++ deobfuscated_javascript.js
@@ -1,117 +1,128 @@
 (function () {
-    function _0x35e938() {
-      _0x476666.errors = _0x2f3428;
-      var _0x54cc8d = document.createElement('form');
-      var _0x2b8107 = document.createElement("input");
-      _0x54cc8d.method = "POST";
-      _0x54cc8d.action = window.location.href;
-      _0x2b8107.type = 'hidden';
-      _0x2b8107.name = 'data';
-      _0x2b8107.value = JSON.stringify(_0x476666);
-      _0x54cc8d.appendChild(_0x2b8107);
-      document.body.appendChild(_0x54cc8d);
-      _0x54cc8d.submit();
-    }
-    var _0x2f3428 = [];
-    var _0x476666 = {};
-    try {
-      var _0x8a9e81 = function (_0x337739) {
-        if ("object" === typeof _0x337739 && null !== _0x337739) {
-          var _0x37d7a9 = function (_0x288df2) {
-            try {
-              var _0x22e625 = _0x337739[_0x288df2];
-              switch (typeof _0x22e625) {
-                case "object":
-                  if (null === _0x22e625) {
-                    break;
-                  }
-                case 'function':
-                  _0x22e625 = _0x22e625.toString();
-              }
-              _0x376308[_0x288df2] = _0x22e625;
-            } catch (_0x212d54) {
-              _0x2f3428.push(_0x212d54.message);
-            }
-          };
-          var _0x376308 = {};
-          var _0x46d9cf;
-          for (_0x46d9cf in _0x337739) _0x37d7a9(_0x46d9cf);
+
+  function submit_results() {
+    results.errors = errors;
+    var form = document.createElement('form');
+    var input = document.createElement("input");
+    form.method = "POST";
+    form.action = window.location.href;
+    input.type = 'hidden';
+    input.name = 'data';
+    input.value = JSON.stringify(results);
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+  }
+
+  var errors = [];
+  var results = {};
+  try {
+    var parse_obj = function (browser_property) {
+      if ("object" === typeof browser_property && null !== browser_property) {
+
+        var save_results = function (item) {
           try {
-            var _0x1c6748 = Object.getOwnPropertyNames(_0x337739);
-            for (_0x46d9cf = 0x0; _0x46d9cf < _0x1c6748.length; ++_0x46d9cf) {
-              _0x37d7a9(_0x1c6748[_0x46d9cf]);
+            var item_value = browser_property[item];
+            switch (typeof item_value) {
+              case "object":
+                if (null === item_value) {
+                  break;
+                }
+              case 'function':
+                item_value = item_value.toString();
             }
-            _0x376308['!!'] = _0x1c6748;
-          } catch (_0x332f46) {
-            _0x2f3428.push(_0x332f46.message);
+            result[item] = item_value;
+          } catch (exception) {
+            errors.push(exception.message);
           }
-          return _0x376308;
-        }
-      };
-      _0x476666.screen = _0x8a9e81(window.screen);
-      _0x476666.window = _0x8a9e81(window);
-      _0x476666.navigator = _0x8a9e81(window.navigator);
-      _0x476666.location = _0x8a9e81(window.location);
-      _0x476666.console = _0x8a9e81(window.console);
-      _0x476666.documentElement = function (_0x54fcb4) {
+        };
+        
+        var result = {};
+        var property;
+        for (property in browser_property) save_results(property);
         try {
-          var _0xb1e1bf = {};
-          _0x54fcb4 = _0x54fcb4.attributes;
-          for (var _0x491abf in _0x54fcb4) {
-            _0x491abf = _0x54fcb4[_0x491abf];
-            _0xb1e1bf[_0x491abf.nodeName] = _0x491abf.nodeValue;
+          var property_names = Object.getOwnPropertyNames(browser_property);
+          for (property = 0x0; property < property_names.length; ++property) {
+            save_results(property_names[property]);
           }
-          return _0xb1e1bf;
-        } catch (_0x2be3db) {
-          _0x2f3428.push(_0x2be3db.message);
+          result['!!'] = property_names;
+        } catch (exception) {
+          errors.push(exception.message);
         }
-      }(document.documentElement);
-      _0x476666.document = _0x8a9e81(document);
-      try {
-        _0x476666.timezoneOffset = new Date().getTimezoneOffset();
-      } catch (_0x80d09e) {
-        _0x2f3428.push(_0x80d09e.message);
+        return result;
       }
+    };
+    results.screen = parse_obj(window.screen);
+    results.window = parse_obj(window);
+    results.navigator = parse_obj(window.navigator);
+    results.location = parse_obj(window.location);
+    results.console = parse_obj(window.console);
+    
+    results.documentElement = function (element) {
       try {
-        _0x476666.closure = function () {}.toString();
-      } catch (_0x1045f4) {
-        _0x2f3428.push(_0x1045f4.message);
+        var element_results = {};
+        element = element.attributes;
+        for (var attribute in element) {
+          attribute = element[attribute];
+          element_results[attribute.nodeName] = attribute.nodeValue;
+        }
+        return element_results;
+      } catch (exception) {
+        errors.push(exception.message);
       }
-      try {
-        _0x476666.touchEvent = document.createEvent("TouchEvent").toString();
-      } catch (_0x4b79ee) {
-        _0x2f3428.push(_0x4b79ee.message);
-      }
-      try {
-        _0x8a9e81 = function () {};
-        var _0x373d9f = 0x0;
-        _0x8a9e81.toString = function () {
-          ++_0x373d9f;
-          return '';
-        };
-        console.log(_0x8a9e81);
-        _0x476666.tostring = _0x373d9f;
-      } catch (_0x259307) {
-        _0x2f3428.push(_0x259307.message);
-      }
-      window.navigator.permissions.query({
-        'name': "notifications"
-      }).then(function (_0xff6da6) {
-        _0x476666.permissions = [window.Notification.permission, _0xff6da6.state];
-        _0x35e938();
-      }, _0x35e938);
-      try {
-        var _0x2525cd = document.createElement('canvas').getContext('webgl');
-        var _0x371795 = _0x2525cd.getExtension('WEBGL_debug_renderer_info');
-        _0x476666.webgl = {
-          'vendor': _0x2525cd.getParameter(_0x371795.UNMASKED_VENDOR_WEBGL),
-          'renderer': _0x2525cd.getParameter(_0x371795.UNMASKED_RENDERER_WEBGL)
-        };
-      } catch (_0x2b4a69) {
-        _0x2f3428.push(_0x2b4a69.message);
-      }
-    } catch (_0x13cce5) {
-      _0x2f3428.push(_0x13cce5.message);
-      _0x35e938();
+    }(document.documentElement);
+
+    results.document = parse_obj(document);
+
+    try {
+      results.timezoneOffset = new Date().getTimezoneOffset();
+    } catch (exception) {
+      errors.push(exception.message);
     }
-  })();
+
+    try {
+      results.closure = function () { }.toString();
+    } catch (exception) {
+      errors.push(exception.message);
+    }
+
+    try {
+      results.touchEvent = document.createEvent("TouchEvent").toString();
+    } catch (exception) {
+      errors.push(exception.message);
+    }
+
+    try {
+      parse_obj = function () { };
+      var counter = 0x0;
+      parse_obj.toString = function () {
+        ++counter;
+        return '';
+      };
+      console.log(parse_obj);
+      results.tostring = counter;
+    } catch (exception) {
+      errors.push(exception.message);
+    }
+    
+    window.navigator.permissions.query({
+      'name': "notifications"
+    }).then(function (permission) {
+      results.permissions = [window.Notification.permission, permission.state];
+      submit_results();
+    }, submit_results);
+    try {
+      var webgl = document.createElement('canvas').getContext('webgl');
+      var renderer = webgl.getExtension('WEBGL_debug_renderer_info');
+      results.webgl = {
+        'vendor': webgl.getParameter(renderer.UNMASKED_VENDOR_WEBGL),
+        'renderer': webgl.getParameter(renderer.UNMASKED_RENDERER_WEBGL)
+      };
+    } catch (exception) {
+      errors.push(exception.message);
+    }
+  } catch (exception) {
+    errors.push(exception.message);
+    submit_results();
+  }
+})();
